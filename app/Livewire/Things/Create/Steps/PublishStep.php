170ba7aa704ce2f93111309605cc439918585680
Alex Garrett-Smith <alex@codecourse.com>
2023-11-28 09:21:44 +0000

    Episode 05

--- app/Livewire/Things/Create/Steps/PublishStep.php
+++ app/Livewire/Things/Create/Steps/PublishStep.php
@@ -2,13 +2,34 @@
 
 namespace App\Livewire\Things\Create\Steps;
 
+use App\Models\Thing;
 use Livewire\Component;
 use Spatie\LivewireWizard\Components\StepComponent;
 
 class PublishStep extends StepComponent
 {
+    public function stateToStore()
+    {
+        return collect($this->state()->all())
+            ->mapWithKeys(fn ($item) => $item)
+            ->only('title', 'description')
+            ->toArray();
+
+        // return [
+        //     'title' => $this->state()->forStep('things.create.steps.title-step')['title'],
+        //     'description' => $this->state()->forStep('things.create.steps.description-step')['description'],
+        // ];
+    }
+
+    public function submit()
+    {
+        Thing::create($this->stateToStore());
+    }
+
     public function render()
     {
-        return view('livewire.things.create.steps.publish-step');
+        return view('livewire.things.create.steps.publish-step', [
+            'state' => $this->stateToStore()
+        ]);
     }
 }
--- resources/views/livewire/things/create/steps/publish-step.blade.php
+++ resources/views/livewire/things/create/steps/publish-step.blade.php
@@ -1,3 +1,14 @@
 <div>
-    {{-- Do your work, then step back. --}}
+    <form class="space-y-6" wire:submit="submit">
+        <div>
+            <ol>
+                <li>Title: {{ $state['title'] }}</li>
+                <li>Description: {{ $state['description'] }}</li>
+            </ol>
+        </div>
+
+        <x-primary-button>
+            {{ __('Publish') }}
+        </x-primary-button>
+    </form>
 </div>
